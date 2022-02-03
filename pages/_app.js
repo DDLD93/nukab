0688a5fc3d84e6d5cf4db54cd4db5b2918c8179b
ddld93 <umar.jere@gmail.com>
2022-02-03 20:14:57 +0100

    the commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  
+    palette: {
+      primary:{
+        main: '#ff9800',
+      }, 
+      secondary: {
+        main: '#004d40',
+      },
+    },
+  
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
